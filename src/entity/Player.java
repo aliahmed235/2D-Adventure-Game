f83d7f7794f6db98bd80c127769fae96167eff4d
aliahmed235 <aliahmed1174@outlook.com>
2026-04-04 06:03:03 +0200

    Fix Character doesnt appear

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -49,14 +49,14 @@ public class Player extends Entity {
 
     public void getPlayerImage() {
         try {
-            up1    = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_up_1.png"));
-            up2    = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_up_2.png"));
-            down1  = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_down_1.png"));
-            down2  = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_down_2.png"));
-            right1 = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_right_1.png"));
-            right2 = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_right_2.png"));
-            left1  = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_left_1.png"));
-            left2  = ImageIO.read(new File("C:/Users/aliah/OneDrive/Desktop/2D_GAME/res/Player/boy_left_2.png"));
+            up1    = ImageIO.read(new File("res/Player/boy_up_1.png"));
+            up2    = ImageIO.read(new File("res/Player/boy_up_2.png"));
+            down1  = ImageIO.read(new File("res/Player/boy_down_1.png"));
+            down2  = ImageIO.read(new File("res/Player/boy_down_2.png"));
+            right1 = ImageIO.read(new File("res/Player/boy_right_1.png"));
+            right2 = ImageIO.read(new File("res/Player/boy_right_2.png"));
+            left1  = ImageIO.read(new File("res/Player/boy_left_1.png"));
+            left2  = ImageIO.read(new File("res/Player/boy_left_2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
